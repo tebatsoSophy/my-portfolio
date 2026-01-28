3ea933409bc54bc5243fdfbc56ac926ab1b5249f
Tebatso Sophy Mahlathini <u22611704@tuks.co.za>
2026-01-28 14:13:17 +0200

    links fixed

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -20,7 +20,7 @@ export const Banner = () => {
     }, delta);
 
     return () => { clearInterval(ticker) };
-  }, [text])
+  }, [text,delta,text])
 
   const tick = () => {
     let i = loopNum % toRotate.length;
